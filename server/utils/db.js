35772f3bce9bb1c160722ce15c756ff4e113bcdd
tanyamediratta <tanyamediratta2@gmail.com>
2025-08-31 19:10:18 +0530

    Commit all unsaved changes

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,13 +1,14 @@
-console.log('MONGO_URI present:', !!process.env.MONGO_URI, 'length:', (process.env.MONGO_URI||'').length);
-await mongoose.connect(process.env.MONGO_URI);
-
 import mongoose from "mongoose";
-
 
 export async function connect() {
   const uri = process.env.MONGO_URI;
   if (!uri) throw new Error("MONGO_URI missing in .env");
+
   mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
+
+  await mongoose.connect(uri, {
+    serverSelectionTimeoutMS: 15000,
+  });
+
   console.log("MongoDB connected");
 }
